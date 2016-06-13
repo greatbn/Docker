08d8fbb86d661475c89670c41c6a2b632e3eb097
great_bn <great_bn@yahoo.com>
2016-06-13 09:17:57 +0700

    env default

--- Dockerfiles/manila-api/config.sh
+++ Dockerfiles/manila-api/config.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+#default value if not set
+
+MANILA_DBADDR=${MANILA_DBADDR-mysql}
+RABBIT_ADDR=${RABBIT_ADDR-rabbitmq}
+
 sed -i 's/HOSTNAME/'$HOSTNAME'/g' /etc/manila/manila.conf
 sed -i 's/MANILA_DBUSER/'$MANILA_DBUSER'/g' /etc/manila/manila.conf
 sed -i 's/MANILA_DBPASS/'$MANILA_DBPASS'/g' /etc/manila/manila.conf
-sed -i 's/MANILADB_ADDR/'$MANILADB_ADDR'/g' /etc/manila/manila.conf
+sed -i 's/MANILA_DBADDR/'$MANILA_DBADDR'/g' /etc/manila/manila.conf
 sed -i 's/MANILA_DBNAME/'$MANILA_DBNAME'/g' /etc/manila/manila.conf
 sed -i 's/CONTROLLER_ADDR/'$CONTROLLER_ADDR'/g' /etc/manila/manila.conf
 sed -i 's/MANILA_USER/'$MANILA_USER'/g' /etc/manila/manila.conf
