ed711cebe1342f771dcbd1073e5ba1721a54735c
greatbn <great_bn@yahoo.com>
2016-07-11 17:01:26 +0700

    keystone on ubuntu ok

--- Dockerfiles/keystone/entrypoint.sh
+++ Dockerfiles/keystone/entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 /home/config.sh
-adduser -S keystone
-addgroup -S keystone
+#adduser -S keystone
+#addgroup -S keystone
+useradd --home-dir /var/lib/keystone --create-home --system --shell /bin/false keystone
+
 mkdir /var/log/keystone/
 keystone-manage db_sync
 keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
-
+echo   "ServerName "`getent hosts ${1:-$HOSTNAME} | awk '{print $1}'` >>  /etc/apache2/apache2.conf
+chown -R keystone:keystone /etc/keystone/*
+chown keystone:keystone /etc/keystone
+chown -R keystone:keystone /var/log/keystone
 echo "[i] Starting daemon..."
 # run apache httpd daemon
 exec /usr/sbin/apachectl -DFOREGROUND;
